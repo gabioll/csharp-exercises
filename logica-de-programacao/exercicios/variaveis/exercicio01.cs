3c3dbb58501263466796e791d109e249993bc48e
Gabrielly Araujo <gabriellyaraujo8733@gmail.com>
2025-10-21 22:59:44 -0300

    Create exercicio01.cs

---
+++ logica-de-programacao/exercicios/variaveis/exercicio01.cs
@@ -0,0 +1,14 @@
+using System;
+namespace LogicaDeProgramacao.Exercicios.Variaveis
+{
+    class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;  
+            
+            
+        }
+    }
+}
